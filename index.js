241462b9af2405d722185b99281ccc8294408592
Dan P <93457949+dpayne5532@users.noreply.github.com>
2022-11-01 01:39:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+
+
+// for (var i = 0; i < 50; i++) {
+//   let dan = Math.floor(Math.random() * 2)
+// console.log(dan ? "True" : "False")
+// }
+
+
+function shouldDanSalsa() {
+  let dan = Math.floor(Math.random() * 2)
+console.log(dan ? "Hell Yes!" : "Fuck No!")
+}
+
+shouldDanSalsa()
